8a35b50cea00965bc562972e214d1e71cca2fe06
Justintochukwu <justintochukwu00@gmail.com>
2025-07-04 21:50:11 +0100

    task

--- task_2.sql
+++ task_2.sql
@@ -28,12 +28,12 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ):
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INIT,
-    quantity INT,
+    quantity DOUBLE,
     subtotal DECIMAL(10,2)
-    FOREIGN KEY (order_id) REFERENCES Order(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 ):
