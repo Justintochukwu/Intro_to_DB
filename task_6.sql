c5306830c1ed7196580ea5d01cfbe49675e6d0ca
Justintochukwu <justintochukwu00@gmail.com>
2025-07-04 22:32:53 +0100

    task6

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
-INSERT INFO customer (customer_id, customer_name, email, address)
+INSERT INtO customer (customer_id, customer_name, email, address)
 VALUES
-(2, 'Blessing Malik', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '125 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
